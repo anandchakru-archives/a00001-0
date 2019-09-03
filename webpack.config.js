e78cc30d6a37ba08b319247bcdea569c11189622
Anand C <anandchakru.forum@gmail.com>
2019-09-02 23:25:08 -0700

    CORS fixed

--- webpack.config.js
+++ webpack.config.js
@@ -52,7 +52,12 @@ module.exports = (env, argv) => {
     devServer: {
       contentBase: argv.mode === 'development' ? niviewPath : prodPath,
       compress: true,
-      watchContentBase: true
+      watchContentBase: true,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+        'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+      }
     }
   }
 }
